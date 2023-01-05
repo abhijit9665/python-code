f09c23c67b570df135c14f712b4e639c527cfd87
abhijit9665 <abhijitbuddhehalkar@gmail.com>
2023-01-05 12:32:07 +0530

    python code added

---
+++ 2.py
@@ -0,0 +1,14 @@
+# a="aaaa"
+# b="sssss"
+# c="aaaa"
+# print( b and a and a ) 
+# print(id(a))
+# print(id(c))
+
+
+list1=['a','b']
+list2=['a','b']
+print(id(list1))
+print(id(list2))
+print(list1 is list2) #difference address..... is operator is address comparision 
+print(list1==list2)
